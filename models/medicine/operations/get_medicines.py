acaff08b7bc76ca2224fe0a1a1cb8592b645d6bf
Neli Harbuzava <ineliharbuzava@gmail.com>
2025-10-05 14:06:55 +0300

    return everything for medicines list

--- models/medicine/operations/get_medicines.py
+++ models/medicine/operations/get_medicines.py
@@ -1,11 +1,12 @@
-from sqlalchemy.orm import noload
+from sqlalchemy import select
+from sqlalchemy.orm import joinedload
 
 from models.medicine.medicine import Medicine
 from services.db.decorators.with_session import with_session
-from sqlalchemy import select
+
 
 @with_session
 def get_medicines(session, user_id):
-    stmt = select(Medicine).order_by(Medicine.id).where(Medicine.user_id.in_([user_id])).options(noload('*'))
+    stmt = select(Medicine).order_by(Medicine.id).where(Medicine.user_id.in_([user_id])).options(joinedload('*'))
     medicines = session.scalars(stmt).all()
     return medicines
