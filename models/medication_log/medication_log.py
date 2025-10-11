7077555ec60098865f56553bf3d11f8694e2c81a
Neli Harbuzava <ineliharbuzava@gmail.com>
2025-10-11 20:35:07 +0300

    add medication log type

--- models/medication_log/medication_log.py
+++ models/medication_log/medication_log.py
@@ -1,17 +1,34 @@
 import datetime
+import enum
 from dataclasses import dataclass
 
 from sqlalchemy import ForeignKey, TIMESTAMP
+from sqlalchemy.dialects.postgresql import ENUM as pgEnum
 from sqlalchemy.orm import Mapped
 from sqlalchemy.orm import mapped_column
 
 from models.base import Base
 
 
+class MedicationLogTypes(str, enum.Enum):
+    taken = "taken"
+    skipped = "skipped"
+
+
+MedicationLogTypesType: pgEnum = pgEnum(
+    MedicationLogTypes,
+    name="medication_log_types",
+    create_constraint=True,
+    metadata=Base.metadata,
+    validate_strings=True,
+)
+
+
 @dataclass
 class MedicationLog(Base):
     __tablename__ = "medication_logs"
     id: Mapped[int] = mapped_column(primary_key=True)
+    type: Mapped[str] = mapped_column(MedicationLogTypesType, nullable=False)
     medicine_id: Mapped[int] = mapped_column(ForeignKey("medicines.id"))
     date: Mapped[datetime.datetime] = mapped_column(TIMESTAMP(timezone=True), nullable=False)
 
--- routers/api/medication_logs.py
+++ routers/api/medication_logs.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, request, jsonify
 
+from models.medication_log.medication_log import MedicationLogTypes
 from models.medication_log.operations.create_medication_log import create_medication_log
 from models.medication_log.validations import TakeMedicationLogValidation
 from services.routers.decorators.token_required import token_required
@@ -15,5 +16,6 @@ def api_medication_logs_take(user, medicine_id):
     # TODO: validate medicine belongs to user
     medication_log_data = request.json
     medication_log_data['medicine_id'] = medicine_id
+    medication_log_data['type'] = MedicationLogTypes.taken
 
     return jsonify(create_medication_log(**medication_log_data))
