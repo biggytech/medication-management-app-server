7174fe24e16c37f5699b6de062e09c54bee20d60
Neli Harbuzava <ineliharbuzava@gmail.com>
2025-10-18 16:35:06 +0300

    adjust validations

--- models/user/validations.py
+++ models/user/validations.py
@@ -1,18 +1,22 @@
-from pydantic import BaseModel, Field, EmailStr
 from typing import Optional
-from datetime import date
+
+from pydantic import BaseModel, Field, EmailStr
+
 from models.common.enums.sex_types import SexTypes
+
 
 class SignInDefaultValidation(BaseModel):
     email: EmailStr
     password: str = Field(min_length=8)
 
+
 class CreateUserValidation(SignInDefaultValidation):
     full_name: str = Field(min_length=1, max_length=255)
+
 
 class UpdateUserValidation(BaseModel):
     full_name: Optional[str] = Field(None, min_length=1, max_length=255)
     email: Optional[EmailStr] = None
     password: Optional[str] = Field(None, min_length=8)
     sex: Optional[SexTypes] = None
-    date_of_birth: Optional[date] = None
+    # date_of_birth: Optional[date] = None
