5c5bb86f0719539052cc1fd53a3ac28d0cd83b01
Neli Harbuzava <ineliharbuzava@gmail.com>
2026-01-08 23:49:59 +0300

    email password reset

--- routers/api/users.py
+++ routers/api/users.py
@@ -1,8 +1,11 @@
 from flask import Blueprint, request, jsonify
 
+from models.user.operations.get_user_by_email import get_user_by_email
 from models.user.operations.get_user_by_id import get_user_by_id
 from models.user.operations.update_user import update_user
 from models.user.validations import UpdateUserValidation
+from services.auth.get_random_password import get_random_password
+from services.email.send_password_reset_report import send_password_reset_report
 from services.routers.decorators.token_required import token_required
 from services.routers.decorators.validate_request import validate_request, BODY
 
@@ -19,6 +22,34 @@ def get_user_profile(user):
     print(user_dict)
 
     return user_dict
+
+
+@api_users.post('/forgot-password')
+# @validate_request(BODY, SignInDefaultValidation)
+def forgot_password():
+    user_data = request.json
+
+    if not user_data:
+        return jsonify({
+            'success': False,
+            'error': 'Request body is required'
+        }), 400
+
+    print("DATAHERE")
+    print(user_data)
+
+    user = get_user_by_email(user_data['email'])
+
+    if not user:
+        return jsonify({'error': 'Invalid email'}), 401
+
+    password = get_random_password()
+
+    update_user(user=user, password=password, email=user.email)
+
+    send_password_reset_report(email=user.email, password=password)
+
+    return jsonify({'message': 'OK'}), 200
 
 
 @api_users.route('/profile', methods=['PUT'])
@@ -38,8 +69,6 @@ def update_user_profile(user):
 
         user = get_user_by_id(user.id)
         user_dict = user.as_dict()
-
-        print(user_dict)
 
         return user_dict
 
---
+++ services/email/send_password_reset_report.py
@@ -0,0 +1,30 @@
+import os
+from datetime import datetime
+from typing import Dict
+
+from flask import current_app
+from flask_mail import Message
+
+from models.doctor.operations.get_doctor_by_id import get_doctor_by_id
+from models.health_tracker_log.operations.get_health_tracker_logs_by_date_range import \
+    get_health_tracker_logs_by_date_range
+from models.medication_log.operations.get_medication_logs_by_date_range import get_medication_logs_by_date_range
+from models.user.operations.get_user_by_id import get_user_by_id
+from services.pdf.generate_patient_report import PatientReportGenerator
+
+
+def send_password_reset_report(email, password):
+    body = f"""Был запрошен доступ на сброс вашего пароля.
+
+Ваш новый пароль {password}
+    """
+
+    msg = Message(
+        subject='Обновление пароля',
+        recipients=[email],
+        body=body,
+        sender=('Медика', current_app.config.get('MAIL_DEFAULT_SENDER', 'noreply@medicationapp.com'))
+    )
+
+    from app import mail
+    mail.send(msg)
